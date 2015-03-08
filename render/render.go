dcf6c01e60dfa8b57d8c60b6149dcc5e264989a6
Shane Logsdon <shane@shanelogsdon.com>
2015-03-07 20:14:56 -0500

    beginnings of HTML templating

--- render/render.go
+++ render/render.go
@@ -2,8 +2,35 @@ package render
 
 import (
 	"encoding/json"
+	"html/template"
 	"io"
 )
+
+var (
+	Config = DefaultConfig()
+	t      *template.Template
+)
+
+type RenderConfig struct {
+	TemplateDir        string
+	TemplateExtensions []string
+}
+
+func init() {
+	pattern := Config.TemplateDir + "/*." + Config.TemplateExtensions[0]
+	t, _ = template.ParseGlob(pattern)
+}
+
+func DefaultConfig() *RenderConfig {
+	return &RenderConfig{
+		TemplateDir:        "./templates",
+		TemplateExtensions: []string{"tmpl"},
+	}
+}
+
+func HTML(w io.Writer, name string, bindings ...interface{}) (err error) {
+	return
+}
 
 func JSON(w io.Writer, data interface{}) (err error) {
 	json, err := json.Marshal(data)
